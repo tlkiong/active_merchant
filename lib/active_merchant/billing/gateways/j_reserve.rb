ceea5760192dd1d7ae93c22ede06beac363fc9ca
Kiong <kiong90@gmail.com>
2019-06-19 02:48:57 +0800

    WIP: Able to trigger purchase but receives error instead

--- lib/active_merchant/billing/gateways/j_reserve.rb
+++ lib/active_merchant/billing/gateways/j_reserve.rb
@@ -1,123 +1,179 @@
+require 'cgi'
+
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     class JReserveGateway < Gateway
-      self.test_url = 'https://example.com/test'
-      self.live_url = 'https://example.com/live'
+      self.test_url = 'https://test.j-reserve.com:10443/ros/settlement.php'
+      self.live_url = 'https://credit.j-reserve.com/ros/settlement.php'
 
       self.supported_countries = ['US']
-      self.default_currency = 'USD'
-      self.supported_cardtypes = [:visa, :master, :american_express, :discover]
+      self.default_currency = 'JPY'
+      self.supported_cardtypes = [:visa, :master, :american_express]
 
       self.homepage_url = 'http://www.example.net/'
-      self.display_name = 'New Gateway'
+      self.display_name = 'J-Reserve'
+      self.money_format = :cents
 
-      STANDARD_ERROR_CODE_MAPPING = {}
+      STANDARD_ERROR_CODE_MAPPING = {
+        'S10000000' => 'A system error has occurred.',
+        'S10000001' => 'Temporary sales processing failed',
+        'S10000002' => 'Cancel processing failed',
+        'S20010001' => 'Processing category (job) not found'
+      }
 
       def initialize(options={})
-        requires!(options, :some_credential, :another_credential)
+        requires!(options, :member_code)
+        @member_code = options[:member_code]
+        raise ArgumentError, "member_code must be a number" unless @member_code.is_a? Numeric
+
         super
       end
 
-      def purchase(money, payment, options={})
-        post = {}
-        add_invoice(post, money, options)
-        add_payment(post, payment)
-        add_address(post, payment, options)
-        add_customer_data(post, options)
-
-        commit('sale', post)
+      def purchase(options)
+        requires!(options, :proposal_code, :card_brand, :card_number, :expire_year, :expire_month, :amount, :cancel_base_fee, :sales_start, :sales_end, :customer_email, :customer_name)
+        post = new_post('AUTH', options)
+        commit(post)
       end
 
-      def authorize(money, payment, options={})
-        post = {}
-        add_invoice(post, money, options)
-        add_payment(post, payment)
-        add_address(post, payment, options)
-        add_customer_data(post, options)
+      # def authorize(money, payment, options={})
+      #   post = {}
+      #   add_invoice(post, money, options)
+      #   add_payment(post, payment)
+      #   add_address(post, payment, options)
+      #   add_customer_data(post, options)
 
-        commit('authonly', post)
-      end
+      #   commit('authonly', post)
+      # end
 
-      def capture(money, authorization, options={})
-        commit('capture', post)
-      end
+      # def capture(money, authorization, options={})
+      #   commit('capture', post)
+      # end
 
-      def refund(money, authorization, options={})
-        commit('refund', post)
-      end
+      # def refund(money, authorization, options={})
+      #   commit('refund', post)
+      # end
 
-      def void(authorization, options={})
-        commit('void', post)
-      end
+      # def void(authorization, options={})
+      #   commit('void', post)
+      # end
 
-      def verify(credit_card, options={})
-        MultiResponse.run(:use_first_response) do |r|
-          r.process { authorize(100, credit_card, options) }
-          r.process(:ignore_result) { void(r.authorization, options) }
-        end
-      end
+      # def verify(credit_card, options={})
+      #   MultiResponse.run(:use_first_response) do |r|
+      #     r.process { authorize(100, credit_card, options) }
+      #     r.process(:ignore_result) { void(r.authorization, options) }
+      #   end
+      # end
 
-      def supports_scrubbing?
-        true
-      end
+      # def supports_scrubbing?
+      #   true
+      # end
 
-      def scrub(transcript)
-        transcript
-      end
+      # def scrub(transcript)
+      #   transcript
+      # end
 
       private
 
-      def add_customer_data(post, options)
-      end
+        # values are:
+        #   {
+        #     member_code: number,
+        #     job: string,
+        #     proposal_code: string,
+        #     card_brand: string,
+        #     card_number: number,
+        #     expire_year: number,
+        #     expire_month: number,
+        #     amount: number,
+        #     cancel_base_fee: number,
+        #     sales_start: string,
+        #     sales_end: string,
+        #     customer_email: string,
+        #     customer_name: string,
+        #   }
+        def new_post(job_type, options)
+          post = {
+            member_code: @member_code,
+            job: job_type
+          }
 
-      def add_address(post, creditcard, options)
-      end
-
-      def add_invoice(post, money, options)
-        post[:amount] = amount(money)
-        post[:currency] = (options[:currency] || currency(money))
-      end
-
-      def add_payment(post, payment)
-      end
-
-      def parse(body)
-        {}
-      end
-
-      def commit(action, parameters)
-        url = (test? ? test_url : live_url)
-        response = parse(ssl_post(url, post_data(action, parameters)))
-
-        Response.new(
-          success_from(response),
-          message_from(response),
-          response,
-          authorization: authorization_from(response),
-          avs_result: AVSResult.new(code: response["some_avs_response_key"]),
-          cvv_result: CVVResult.new(response["some_cvv_response_key"]),
-          test: test?,
-          error_code: error_code_from(response)
-        )
-      end
-
-      def success_from(response)
-      end
-
-      def message_from(response)
-      end
-
-      def authorization_from(response)
-      end
-
-      def post_data(action, parameters = {})
-      end
-
-      def error_code_from(response)
-        unless success_from(response)
-          # TODO: lookup error code for this response
+          options.merge(post)
         end
-      end
+
+        # def add_customer_data(post, options)
+        # end
+
+        # def add_address(post, creditcard, options)
+        # end
+
+        # # def add_invoice(post, money, options)
+        # #   post[:amount] = amount(money)
+        # #   post[:currency] = (options[:currency] || currency(money))
+        # # end
+
+        # def add_payment(post, payment)
+        # end
+
+        # def parse(body)
+        #   {}
+        # end
+
+        def commit(parameters)
+          url = (test? ? test_url : live_url)
+          params = parameters.to_json
+          response = ssl_post(url, params, request_headers)
+
+          # Example of response will be: 
+          #     result=0&order_code=&error_type=S&error_code=S20&error_info=S20010001
+          response_hash = CGI::parse(response)
+
+          Response.new(
+            success_from(response_hash),
+            message_from(response_hash),
+            response_hash,
+            # authorization: authorization_from(response),
+            # avs_result: AVSResult.new(code: response["some_avs_response_key"]),
+            # cvv_result: CVVResult.new(response["some_cvv_response_key"]),
+            test: test?,
+            error_code: error_code_from(response_hash)
+          )
+        end
+
+        def success_from(response_hash)
+          response_hash['result'].first === '1' # Value is either 0 / 1. 0 means have error
+        end
+
+        def message_from(response_hash)
+          if success_from(response_hash)
+            # 
+          else
+            error_message_from(response_hash)
+          end
+        end
+
+        # def authorization_from(response)
+        # end
+
+        # def post_data(action, parameters = {})
+        #   JSON.generate(parameters)
+        # end
+
+        def request_headers()
+          {
+            'Content-Type' => 'application/json',
+          }
+        end
+
+        def error_message_from(response_hash)
+          unless success_from(response_hash)
+            STANDARD_ERROR_CODE_MAPPING[response_hash['error_info'].first]
+          end
+        end
+
+        def error_code_from(response_hash)
+          unless success_from(response_hash)
+            response_hash['error_info'].first
+          end
+        end
     end
   end
 end
--- test/remote/gateways/remote_j_reserve_test.rb
+++ test/remote/gateways/remote_j_reserve_test.rb
@@ -3,145 +3,161 @@ require 'test_helper'
 class RemoteJReserveTest < Test::Unit::TestCase
   def setup
     @gateway = JReserveGateway.new(fixtures(:j_reserve))
+    JReserveGateway.ssl_strict = false
 
-    @amount = 100
-    @credit_card = credit_card('4000100011112224')
-    @declined_card = credit_card('4000300011112220')
-    @options = {
-      billing_address: address,
-      description: 'Store Purchase'
+    @test_val = {
+      :card => {
+        :number => 4111111111111111,
+        :expire_year => default_expiration_date.year,
+        :expire_month => default_expiration_date.month
+      }
     }
   end
 
   def test_successful_purchase
-    response = @gateway.purchase(@amount, @credit_card, @options)
-    assert_success response
-    assert_equal 'REPLACE WITH SUCCESS MESSAGE', response.message
-  end
-
-  def test_successful_purchase_with_more_options
-    options = {
-      order_id: '1',
-      ip: "127.0.0.1",
-      email: "joe@example.com"
+    purchase_options = {
+      :proposal_code => "testCode1",
+      :card_brand => "VISA",
+      :card_number => @test_val[:card][:number],
+      :expire_year => @test_val[:card][:expire_year],
+      :expire_month => @test_val[:card][:expire_month],
+      :amount => 1000,
+      :cancel_base_fee => 100,
+      :sales_start => (Date.today + 1).strftime("%Y-%m-%d"),
+      :sales_end => (Date.today + 2).strftime("%Y-%m-%d"),
+      :customer_email => "test@gmail.com",
+      :customer_name => "Test User"
     }
-
-    response = @gateway.purchase(@amount, @credit_card, options)
+    
+    response = @gateway.purchase(purchase_options)
     assert_success response
     assert_equal 'REPLACE WITH SUCCESS MESSAGE', response.message
   end
 
-  def test_failed_purchase
-    response = @gateway.purchase(@amount, @declined_card, @options)
-    assert_failure response
-    assert_equal 'REPLACE WITH FAILED PURCHASE MESSAGE', response.message
-  end
+  # def test_successful_purchase_with_more_options
+  #   options = {
+  #     order_id: '1',
+  #     ip: "127.0.0.1",
+  #     email: "joe@example.com"
+  #   }
 
-  def test_successful_authorize_and_capture
-    auth = @gateway.authorize(@amount, @credit_card, @options)
-    assert_success auth
+  #   response = @gateway.purchase(@amount, @credit_card, options)
+  #   assert_success response
+  #   assert_equal 'REPLACE WITH SUCCESS MESSAGE', response.message
+  # end
 
-    assert capture = @gateway.capture(@amount, auth.authorization)
-    assert_success capture
-    assert_equal 'REPLACE WITH SUCCESS MESSAGE', capture.message
-  end
+  # TODO:
+  # def test_failed_purchase
+  #   response = @gateway.purchase(@amount, @declined_card, @options)
+  #   assert_failure response
+  #   assert_equal 'REPLACE WITH FAILED PURCHASE MESSAGE', response.message
+  # end
 
-  def test_failed_authorize
-    response = @gateway.authorize(@amount, @declined_card, @options)
-    assert_failure response
-    assert_equal 'REPLACE WITH FAILED AUTHORIZE MESSAGE', response.message
-  end
+  # def test_successful_authorize_and_capture
+  #   auth = @gateway.authorize(@amount, @credit_card, @options)
+  #   assert_success auth
 
-  def test_partial_capture
-    auth = @gateway.authorize(@amount, @credit_card, @options)
-    assert_success auth
+  #   assert capture = @gateway.capture(@amount, auth.authorization)
+  #   assert_success capture
+  #   assert_equal 'REPLACE WITH SUCCESS MESSAGE', capture.message
+  # end
 
-    assert capture = @gateway.capture(@amount-1, auth.authorization)
-    assert_success capture
-  end
+  # def test_failed_authorize
+  #   response = @gateway.authorize(@amount, @declined_card, @options)
+  #   assert_failure response
+  #   assert_equal 'REPLACE WITH FAILED AUTHORIZE MESSAGE', response.message
+  # end
 
-  def test_failed_capture
-    response = @gateway.capture(@amount, '')
-    assert_failure response
-    assert_equal 'REPLACE WITH FAILED CAPTURE MESSAGE', response.message
-  end
+  # def test_partial_capture
+  #   auth = @gateway.authorize(@amount, @credit_card, @options)
+  #   assert_success auth
 
-  def test_successful_refund
-    purchase = @gateway.purchase(@amount, @credit_card, @options)
-    assert_success purchase
+  #   assert capture = @gateway.capture(@amount-1, auth.authorization)
+  #   assert_success capture
+  # end
 
-    assert refund = @gateway.refund(@amount, purchase.authorization)
-    assert_success refund
-    assert_equal 'REPLACE WITH SUCCESSFUL REFUND MESSAGE', refund.message
-  end
+  # def test_failed_capture
+  #   response = @gateway.capture(@amount, '')
+  #   assert_failure response
+  #   assert_equal 'REPLACE WITH FAILED CAPTURE MESSAGE', response.message
+  # end
 
-  def test_partial_refund
-    purchase = @gateway.purchase(@amount, @credit_card, @options)
-    assert_success purchase
+  # def test_successful_refund
+  #   purchase = @gateway.purchase(@amount, @credit_card, @options)
+  #   assert_success purchase
 
-    assert refund = @gateway.refund(@amount-1, purchase.authorization)
-    assert_success refund
-  end
+  #   assert refund = @gateway.refund(@amount, purchase.authorization)
+  #   assert_success refund
+  #   assert_equal 'REPLACE WITH SUCCESSFUL REFUND MESSAGE', refund.message
+  # end
 
-  def test_failed_refund
-    response = @gateway.refund(@amount, '')
-    assert_failure response
-    assert_equal 'REPLACE WITH FAILED REFUND MESSAGE', response.message
-  end
+  # def test_partial_refund
+  #   purchase = @gateway.purchase(@amount, @credit_card, @options)
+  #   assert_success purchase
 
-  def test_successful_void
-    auth = @gateway.authorize(@amount, @credit_card, @options)
-    assert_success auth
+  #   assert refund = @gateway.refund(@amount-1, purchase.authorization)
+  #   assert_success refund
+  # end
 
-    assert void = @gateway.void(auth.authorization)
-    assert_success void
-    assert_equal 'REPLACE WITH SUCCESSFUL VOID MESSAGE', void.message
-  end
+  # def test_failed_refund
+  #   response = @gateway.refund(@amount, '')
+  #   assert_failure response
+  #   assert_equal 'REPLACE WITH FAILED REFUND MESSAGE', response.message
+  # end
 
-  def test_failed_void
-    response = @gateway.void('')
-    assert_failure response
-    assert_equal 'REPLACE WITH FAILED VOID MESSAGE', response.message
-  end
+  # def test_successful_void
+  #   auth = @gateway.authorize(@amount, @credit_card, @options)
+  #   assert_success auth
 
-  def test_successful_verify
-    response = @gateway.verify(@credit_card, @options)
-    assert_success response
-    assert_match %r{REPLACE WITH SUCCESS MESSAGE}, response.message
-  end
+  #   assert void = @gateway.void(auth.authorization)
+  #   assert_success void
+  #   assert_equal 'REPLACE WITH SUCCESSFUL VOID MESSAGE', void.message
+  # end
 
-  def test_failed_verify
-    response = @gateway.verify(@declined_card, @options)
-    assert_failure response
-    assert_match %r{REPLACE WITH FAILED PURCHASE MESSAGE}, response.message
-  end
+  # def test_failed_void
+  #   response = @gateway.void('')
+  #   assert_failure response
+  #   assert_equal 'REPLACE WITH FAILED VOID MESSAGE', response.message
+  # end
 
-  def test_invalid_login
-    gateway = JReserveGateway.new(login: '', password: '')
+  # def test_successful_verify
+  #   response = @gateway.verify(@credit_card, @options)
+  #   assert_success response
+  #   assert_match %r{REPLACE WITH SUCCESS MESSAGE}, response.message
+  # end
 
-    response = gateway.purchase(@amount, @credit_card, @options)
-    assert_failure response
-    assert_match %r{REPLACE WITH FAILED LOGIN MESSAGE}, response.message
-  end
+  # def test_failed_verify
+  #   response = @gateway.verify(@declined_card, @options)
+  #   assert_failure response
+  #   assert_match %r{REPLACE WITH FAILED PURCHASE MESSAGE}, response.message
+  # end
 
-  def test_dump_transcript
-    # This test will run a purchase transaction on your gateway
-    # and dump a transcript of the HTTP conversation so that
-    # you can use that transcript as a reference while
-    # implementing your scrubbing logic.  You can delete
-    # this helper after completing your scrub implementation.
-    dump_transcript_and_fail(@gateway, @amount, @credit_card, @options)
-  end
+  # def test_invalid_login
+  #   gateway = JReserveGateway.new(login: '', password: '')
 
-  def test_transcript_scrubbing
-    transcript = capture_transcript(@gateway) do
-      @gateway.purchase(@amount, @credit_card, @options)
-    end
-    transcript = @gateway.scrub(transcript)
+  #   response = gateway.purchase(@amount, @credit_card, @options)
+  #   assert_failure response
+  #   assert_match %r{REPLACE WITH FAILED LOGIN MESSAGE}, response.message
+  # end
 
-    assert_scrubbed(@credit_card.number, transcript)
-    assert_scrubbed(@credit_card.verification_value, transcript)
-    assert_scrubbed(@gateway.options[:password], transcript)
-  end
+  # def test_dump_transcript
+  #   # This test will run a purchase transaction on your gateway
+  #   # and dump a transcript of the HTTP conversation so that
+  #   # you can use that transcript as a reference while
+  #   # implementing your scrubbing logic.  You can delete
+  #   # this helper after completing your scrub implementation.
+  #   dump_transcript_and_fail(@gateway, @amount, @credit_card, @options)
+  # end
+
+  # def test_transcript_scrubbing
+  #   transcript = capture_transcript(@gateway) do
+  #     @gateway.purchase(@amount, @credit_card, @options)
+  #   end
+  #   transcript = @gateway.scrub(transcript)
+
+  #   assert_scrubbed(@credit_card.number, transcript)
+  #   assert_scrubbed(@credit_card.verification_value, transcript)
+  #   assert_scrubbed(@gateway.options[:password], transcript)
+  # end
 
 end
